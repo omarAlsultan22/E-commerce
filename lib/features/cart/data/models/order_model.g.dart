2c4d8bbf39ab2620541266001373fad51d814ec0
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-02-25 21:23:50 +0200

    Initial commit

---
+++ lib/features/cart/data/models/order_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OrderModelAdapter extends TypeAdapter<OrderModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  OrderModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OrderModel(
+      order: fields[0] as String,
+      image: fields[1] as String,
+      price: fields[2] as int,
+      item: fields[3] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OrderModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.order)
+      ..writeByte(1)
+      ..write(obj.image)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.item);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OrderModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
